9dcd98809f8845c60b83c72c1ce57a26d374eee8
RISA RISKI AMALIA - <19102079@ittelkom-pwt.ac.id>
2022-04-14 19:39:55 +0700

    Modul 3

---
+++ app/src/main/java/com/risarilia_19102079/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.risarilia_19102079.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
